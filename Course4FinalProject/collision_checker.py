fix: corrected the thetaf() func; still need to debug the score calculation when dealing with multiple paths

Course4FinalProject/collision_checker.py
@@ -86,11 +86,6 @@ class CollisionChecker:
                 circle_locations[:, 0] = path[0][j] + np.array(self._circle_offsets) * cos(path[2][j])
                 circle_locations[:, 1] = path[1][j] + np.array(self._circle_offsets) * sin(path[2][j])
 
-                # print(path[0][j])
-                # print(circle_locations)
-                # print(self._circle_offsets)
-                # print(path[2][j])
-
                 # Assumes each obstacle is approximated by a collection of
                 # points of the form [x, y].
                 # Here, we will iterate through the obstacle points, and check
@@ -110,12 +105,8 @@ class CollisionChecker:
                 if not collision_free:
                     break
 
-            # if (not collision_free):
-                # print("WOWOWOWOWOWOWOWOWO!!!!!!!!!!!!")
-
             collision_check_array[i] = collision_free
-        print("------------------------")
-        print(collision_check_array)
+
         return collision_check_array
 
     ######################################################
@@ -178,8 +169,6 @@ class CollisionChecker:
                 y_last = paths[i][1][-1]
                 score = sqrt( (x_goal - x_last) ** 2 + (y_goal - y_last) ** 2 )
 
-                # print("i = " , i, ": score(before) = ", score)
-
                 # Compute the "proximity to other colliding paths" score and
                 # add it to the "distance from centerline" score.
                 # The exact choice of objective function is up to you.
@@ -193,7 +182,7 @@ class CollisionChecker:
                             print("found a collision-possible path")
                             score += - self._weight * sqrt( np.linalg.norm(paths[i][0] - paths[j][0]) ** 2 + \
                                      np.linalg.norm(paths[i][1] - paths[j][1]) ** 2 )
-                # print(": score(after) = ", score)
+
             # Handle the case of colliding paths.
             else:
                 score = float('Inf')
@@ -202,6 +191,5 @@ class CollisionChecker:
             if score < best_score:
                 best_score = score
                 best_index = i
-            # print("best_index = ", best_index, ", best_score = ", best_score)
-            # print("----------------------------------")
+
         return best_index

Course4FinalProject/local_planner.py
@@ -168,10 +168,6 @@ class LocalPlanner:
                                    goal_t, 
                                    goal_v])
 
-        print("=====================")
-        print(goal_state)
-        print(goal_state_set)
-
         return goal_state_set
   
     # Plans the path set using polynomial spiral optimization to
@@ -216,6 +212,7 @@ class LocalPlanner:
             path = self._path_optimizer.optimize_spiral(goal_state[0], 
                                                         goal_state[1], 
                                                         goal_state[2])
+
             if np.linalg.norm([path[0][-1] - goal_state[0], 
                                path[1][-1] - goal_state[1], 
                                path[2][-1] - goal_state[2]]) > 0.1:
@@ -227,7 +224,7 @@ class LocalPlanner:
         return paths, path_validity
 
 def transform_paths(paths, ego_state):
-    """ Converts the to the global coordinate frame.
+    """ Converts to the global coordinate frame.
 
     Converts the paths from the local (vehicle) coordinate frame to the
     global coordinate frame.
@@ -271,5 +268,6 @@ def transform_paths(paths, ego_state):
             t_transformed.append(path[2][i] + ego_state[2])
 
         transformed_paths.append([x_transformed, y_transformed, t_transformed])
+
 
     return transformed_paths

Course4FinalProject/path_optimizer.py
@@ -112,8 +112,11 @@ class PathOptimizer:
         # TODO(done)
         # Yusen's Note: confirm if we have comment pass
         # Remember that a, b, c, d and s are lists
-        thetas = a + np.multiply(b, s) + np.multiply( c, np.power(s, 2) ) + \
-                     np.multiply( d, np.power(s, 3) )
+        thetas = np.multiply( a, s ) + \
+                 np.multiply( b/2, np.power(s, 2) ) + \
+                 np.multiply( c/3, np.power(s, 3) ) + \
+                 np.multiply( d/4, np.power(s, 4) )
+
         return thetas
 
     ######################################################
@@ -168,8 +171,8 @@ class PathOptimizer:
         t_points = self.thetaf(a, b, c, d, s_points)
 
         # Integrate for x & y
-        x_points = scipy.integrate.cumtrapz(np.cos(t_points), s_points, initial=0)
-        y_points = scipy.integrate.cumtrapz(np.sin(t_points), s_points, initial=0)
+        x_points = scipy.integrate.cumtrapz(np.cos(t_points), s_points, initial=0.0)
+        y_points = scipy.integrate.cumtrapz(np.sin(t_points), s_points, initial=0.0)
         return [x_points, y_points, t_points]
 
     ######################################################

Course4FinalProject/test_path_optimizer.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 
 po = path_optimizer.PathOptimizer()
 
-path = po.optimize_spiral([9.185], [-4.499], 4.261)
+# path = po.optimize_spiral(9.185, -4.499, 4.261)
+path = po.optimize_spiral(16.49, -3.01, 0.0)
 # path = po.optimize_spiral(0.0, 0.0, 10.22874134679308) # NAN result
 
 for x,y,t in np.stack(arrays=path, axis=1):